1c0188f3168f15e40ef75663c0be3c9807721eb2
David Reynolds <david@reynoldsfamily.org.uk>
2013-05-02 09:34:25 +0100

    Add random vaguely amusing success and failure messages

--- ncplugin.py
+++ ncplugin.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 from pync import Notifier
 from nose.plugins import Plugin
@@ -8,6 +9,26 @@ class NCPlugin(Plugin):
     name = 'NCPlugin'
     enabled = True
 
+    success_messages = [
+        'Hurrah!',
+        'Awesome!',
+        'Good work!',
+        'Well done!',
+    ]
+
+    failure_messages = [
+        'Boo!',
+        'Doh!',
+    ]
+
+    @property
+    def failure_msg(self):
+        return random.choice(self.failure_messages)
+
+    @property
+    def success_msg(self):
+        return random.choice(self.success_messages)
+
     def finalize(self, result):
         failures = len(result.failures)
         errors = len(result.errors)
@@ -16,9 +37,9 @@ class NCPlugin(Plugin):
 
         if result.wasSuccessful():
             title = "Tests passed in: %s" % folder
-            msg = 'Hurrah! All of your %s test(s) passed!' % result.testsRun
+            msg = '%s All of your %s test(s) passed!' % (self.success_msg, result.testsRun)
         else:
-            msg = 'Boo! %s failures, %s errors and %s skips' % (failures, errors, skips)
+            msg = '%s %s failures, %s errors from %s tests' % (self.failure_msg, failures, errors, result.testsRun)
             title = "Tests failed in: %s" % folder
 
         Notifier.notify(msg, title=title, group='%s_%s_tests' % (folder, self.name))
